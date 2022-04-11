510064c3f30db2fbd96d4bf8c00a4ce775936591
Gerry Bong <ggbong734@gmail.com>
2022-04-10 20:44:03 -0700

    added login page, added ability on backend to check if user is logged in and authenticated, added functionality to redirect user to home page when logged in and trying to access /auth

--- src/components/firebaseAuth.tsx
+++ src/components/firebaseAuth.tsx
@@ -1,6 +1,37 @@
-// import { FunctionComponent, useState, useEffect } from "react";
-// import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-// import firebase from "firebase/app";
-// import "firebase/auth";
+import { FunctionComponent, useState, useEffect } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase/app";
+import "firebase/auth";
 
-export {};
+const firebaseAuthConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+        {
+            provider:firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            requireDisplayName: false,
+        }
+    ],
+    signInSuccessUrl: "/",
+};
+
+const FirebaseAuth: FunctionComponent = () => {
+    const [renderAuth, setRenderAuth] = useState(false);
+
+    // have useEffect that runs once on component mount, by setting dependency as []
+    useEffect(() => {
+        setRenderAuth(true);
+    }, []);
+
+    return (
+        <div className="mt-16">
+            {renderAuth ? (<StyledFirebaseAuth
+            uiConfig={firebaseAuthConfig}
+            firebaseAuth={firebase.auth()}>
+            </StyledFirebaseAuth>)
+            :
+             null}
+        </div>
+    )
+};
+
+export default FirebaseAuth;
