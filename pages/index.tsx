ce8babdd3926be5760ce7188e9bf36e680753691
Gerry Bong <ggbong734@gmail.com>
2022-04-10 23:47:41 -0700

    split homepage into two divs, houselist and map, and rendered map component using mapbox react gl wrapper

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@
 // import { useQuery, gql } from "@apollo/client";
 // import { useDebounce } from "use-debounce";
 import Layout from "src/components/layout";
-// import Map from "src/components/map";
+import Map from "src/components/map";
 // import HouseList from "src/components/houseList";
 // import { useLastData } from "src/utils/useLastData";
 // import { useLocalState } from "src/utils/useLocalState";
@@ -11,5 +11,15 @@ import Layout from "src/components/layout";
 // layout is a component that receives a prop div
 // this component is a subcomponent that lives in the _app file
 export default function Home() {
-  return <Layout main={<div>Home</div>} />;
+  return <Layout main={
+    <div className="flex">
+      <div
+        className="w-1/2 pb-4"
+        style={{ maxHeight: "calc(100vh - 64px)", overflowX: "scroll" }}>
+        HouseList
+      </div>
+      <div className="w-1/2 ">
+        <Map />
+      </div>
+    </div>} />;
 }
