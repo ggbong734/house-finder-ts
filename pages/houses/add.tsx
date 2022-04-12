f0f8e1a76813be641bee14a54decee966e4c676c
Gerry Bong <ggbong734@gmail.com>
2022-04-11 23:00:49 -0700

    started on add house form

--- pages/houses/add.tsx
+++ pages/houses/add.tsx
@@ -1,8 +1,21 @@
 // import { GetServerSideProps, NextApiRequest } from "next";
 // import { loadIdToken } from "src/auth/firebaseAdmin";
-// import Layout from "src/components/layout";
-// import HouseForm from "src/components/houseForm";
+import Layout from "src/components/layout";
+import HouseForm from "src/components/houseForm";
+
+import { GetServerSideProps, NextApiRequest } from "next";
+import { loadIdToken } from "src/auth/firebaseAdmin";
 
 export default function Add() {
-  return <div>Add</div>;
+  return <Layout main={<HouseForm />} />;
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const uid = await loadIdToken(req as NextApiRequest);
+  if (!uid) { // if no user id they are not authenticated so we push them to the auth page
+    res.setHeader("location", "/auth");
+    res.statusCode = 302;
+    res.end();
+  }
+  return { props: {} };
 }
