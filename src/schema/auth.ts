f56852b6bbb5ec459535396f683fc94c8a59a481
Gerry Bong <ggbong734@gmail.com>
2022-05-31 23:45:53 -0700

    up to authChecker for graphQL complete

--- src/schema/auth.ts
+++ src/schema/auth.ts
@@ -1,5 +1,8 @@
-// import { AuthChecker } from "type-graphql";
-// import { Context } from "./context";
+import { AuthChecker } from "type-graphql";
+import { Context } from "./context";
 
 // where we will build all the graphQL functionality: resolvers, mutations, queries,
-export { };
+export const authChecker: AuthChecker<Context> = ({ context }) => {
+  const { uid } = context;
+  return !!uid;
+};
