8245cba750f92d2b94fa9a063af2fb9d1027f2e4
Gerry Bong <ggbong734@gmail.com>
2022-06-01 22:57:10 -0700

    added apollo client to front end and wrapped app in apollo provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-// import { ApolloProvider } from "@apollo/client";
-// import { useApollo } from "src/apollo";
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "src/apollo";
 import { AuthProvider } from "src/auth/useAuth";
 import "../styles/index.css";
 
@@ -9,14 +9,20 @@ import "../styles/index.css";
 // component is received as a prop and returned represents each page basically
 // like react router
 // we would want providers, things that are providing to every page of our application like apollo and auth provider
+//we also want our apollo client on every page so we can include the apollo provider here.
+// so we can execute apollo graphql query
+
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const client = useApollo();
   return (
-  <AuthProvider>
-    <Head>
-      <title>Home Sweet Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Component {...pageProps} />
-  </AuthProvider>
+    <AuthProvider>
+      <ApolloProvider client={client}>
+        <Head>
+          <title>Home Sweet Home</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </AuthProvider>
   );
 }
