be6e2c14bdb1753374273ae544814c8cfc66e45e
Gerry Bong <ggbong734@gmail.com>
2022-06-03 22:14:52 -0700

    added mutation that tweaked schema, created imageSignature mutation, added imageresolver to schema, modified houseForm to call our mutation and upload the image to Cloudinary

--- src/schema/image.ts
+++ src/schema/image.ts
@@ -1,11 +1,36 @@
-// import {
-//   ObjectType,
-//   Field,
-//   Int,
-//   Resolver,
-//   Mutation,
-//   Authorized,
-// } from "type-graphql";
-// const cloudinary = require("cloudinary").v2;
+import {
+  ObjectType,
+  Field,
+  Int,
+  Resolver,
+  Mutation,
+  Authorized,
+} from "type-graphql";
+const cloudinary = require("cloudinary").v2;
 
-export {};
+//what is returned from the mutation (get request in a RESTful route, mutation is like a POST or PUT)
+
+@ObjectType()
+class ImageSignature {
+  @Field((_type) => String)
+  signature!: string; // bang is needed to avoid declaration error
+
+  @Field((_type) => Int)
+  timestamp!: number;
+}
+
+//resolver is where we define query and mutation
+export class ImageResolver {
+  @Authorized() // only returns if user if authenticated
+  @Mutation((_returns) => ImageSignature)
+  createImageSignature(): ImageSignature {
+    const timestamp = Math.round(new Date().getTime() / 1000);
+    const signature: string = cloudinary.utils.api_sign_request(
+      {
+        timestamp,
+      },
+      process.env.CLOUDINARY_SECRET
+    );
+    return { timestamp, signature };
+  }
+}
