00585173bde748ce3ae623b912d3e278ffa2b68d
Gerry Bong <ggbong734@gmail.com>
2022-06-27 21:20:45 -0700

    searchBox update

--- src/components/searchBox.tsx
+++ src/components/searchBox.tsx
@@ -13,7 +13,7 @@ import {
     ComboboxOption,
 } from "@reach/combobox";
 import "@reach/combobox/styles.css";
-
+// new
 interface ISearchBoxProps {
     onSelectAddress: (address: string, latitude: number | null, longitude: number | null) => void;
     defaultValue: string;
